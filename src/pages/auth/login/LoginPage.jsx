eaabf33785402f6a6e2986ccbf8188d51a09a6ad
thasine2k1 <thaihoang03122001@gmail.com>
2023-06-15 09:41:38 +0700

    updatenewlogin

--- src/pages/auth/login/LoginPage.jsx
+++ src/pages/auth/login/LoginPage.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import "./login.css";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
-    return (
-        <div>LoginPage</div>
-    )
-}
+  return (
+    <div className="backgroundLogin">
 
-export default LoginPage
+    <div className="login">
+        <form>
+          <h2>Login</h2>
+          <div className="inputBox">
+            <input type="text" required placeholder="Email" />
+          </div>
+          <div className="inputBox">
+            <input type="password" required placeholder="Password" />
+          </div>
+          <div className="inputBox">
+            <input type="submit" value="Login" />
+          </div>
+          <p>
+            Create new accout <Link to="/register">Register</Link>
+          </p>
+        </form>
+        
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
--- src/pages/auth/register/RegisterPage.jsx
+++ src/pages/auth/register/RegisterPage.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
-    return (
-        <div>RegisterPage</div>
-    )
-}
+  return (
+    <div className="backgroundLogin">
+      <div className="login">
+        <form>
+          <h2>New Account</h2>
+          <div className="inputBox">
+            <input type="text" required placeholder="Email" />
+          </div>
+          <div className="inputBox">
+            <input type="password" required placeholder="Create Password" />
+          </div>
+          <div className="inputBox">
+            <input type="password" required placeholder="Confirm Password" />
+          </div>
+          <div className="inputBox">
+            <input type="submit" value="Create" />
+          </div>
+          <p>
+            Already have an account? <Link to='/login'>Login</Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default RegisterPage
+export default RegisterPage;
