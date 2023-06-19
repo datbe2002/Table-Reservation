0f89cd12c909b72777f91a4c67dbe90b52164ca0
Doan Hoang Huy <92454872+HwiHwy@users.noreply.github.com>
2023-06-19 23:20:23 +0700

    UserProfile_Edited

--- src/pages/main/profile/Profile.jsx
+++ src/pages/main/profile/Profile.jsx
@@ -1,36 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
+import profileImage from './profileImage.png'; // Import the profile image file
+
 import './Profile.css'; // Import the custom CSS file for styling
 
-// const UserProfile = ({ userID, name, email, password, phone, role, status }) => {
-    const UserProfile = () => {
+const UserProfile = () => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [name, setName] = useState('Huy depzai');
+  const [email, setEmail] = useState('huydepzai@emdeplem.com');
+  const [phone, setPhone] = useState('031231312312');
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = () => {
+    setIsEditing(false);
+  };
+
+  const handleCancel = () => {
+    setIsEditing(false);
+  };
 
   return (
     <div className="user-profile">
       <h2>User Profile</h2>
       <div className="profile-info">
-        <div className="field">
-          <label>User ID:</label>
-          <span>ID12321</span>
+        <div className="profile-image">
+          <img src={profileImage} alt="Profile" />
         </div>
         <div className="field">
           <label>Name:</label>
-          <span>Huy depzai</span>
+          {isEditing ? (
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          ) : (
+            <span>{name}</span>
+          )}
         </div>
         <div className="field">
           <label>Email:</label>
-          <span>huydepzai@emdeplem.com</span>
-        </div>
-        <div className="field">
-          <label>Password:</label>
-          <span>**********</span>
+          {isEditing ? (
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          ) : (
+            <span>{email}</span>
+          )}
         </div>
         <div className="field">
           <label>Phone:</label>
-          <span>031231312312</span>
+          {isEditing ? (
+            <input
+              type="text"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          ) : (
+            <span>{phone}</span>
+          )}
         </div>
+        {isEditing ? (
+          <div className="button-group">
+            <button className="save-button" onClick={handleSave}>
+              Save
+            </button>
+            <button className="cancel-button" onClick={handleCancel}>
+              Cancel
+            </button>
+          </div>
+        ) : (
+          <button className="edit-button" onClick={handleEdit}>
+            Edit
+          </button>
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default UserProfile;
