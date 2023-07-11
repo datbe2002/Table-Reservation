bb92e0c2266f8da2b50deb7af804dd0e2f1eb11b
Kayt <trieuphamkt59@gmail.com>
2023-07-11 11:08:52 +0700

    update header, navigation

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import Navigation from "../../components/navigation/Navigation";
+import { useSelector } from "react-redux";
 
 const user = { id: 2, role: "user", name: "aaa" };
 const Header = () => {
+  const user = useSelector((state) => state.auth.userDTO);
   return (
     <div className="header">
       <div className="logo">
-        <img></img>
+        <div className="logo-name">SAVORY BITE</div>
+        <div className="logo-slogan">Buon appetito!</div>
       </div>
 
-      <Navigation userID={user.id} />
+      <Navigation userID={user._id} />
     </div>
   );
 };
